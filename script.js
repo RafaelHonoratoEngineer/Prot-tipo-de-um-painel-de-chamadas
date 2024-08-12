b499804d952eab49e7139889070bc866ec2df0fa
RafaelHonoratoEngineer <rafael.honorato@sou.ufmt.br>
2024-08-12 14:05:46 -0400

    Versão Teste

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('dadosPaciente').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const senha = document.getElementById('senha').value;
+    const ambulatorio = document.getElementById('ambulatorio').value;
+    const urgencia = document.getElementById('urgencia').value;
+
+    localStorage.setItem('nome', nome);
+    localStorage.setItem('senha', senha);
+    localStorage.setItem('ambulatorio', ambulatorio);
+    localStorage.setItem('urgencia', urgencia);
+
+    window.open('painelChamada.html', '_blank');
+});
