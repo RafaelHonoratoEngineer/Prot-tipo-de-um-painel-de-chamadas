3d9cb7ab4bd632b1cbf810ff857844d2c3caa950
RafaelHonoratoEngineer <rafael.honorato@sou.ufmt.br>
2024-08-12 14:32:50 -0400

    Adicionando o campos especialidade

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ document.getElementById('dadosPaciente').addEventListener('submit', function(eve
     const nome = document.getElementById('nome').value;
     const senha = document.getElementById('senha').value;
     const ambulatorio = document.getElementById('ambulatorio').value;
+    const especialidade = document.getElementById('especialidade').value;
     const urgencia = document.getElementById('urgencia').value;
 
     localStorage.setItem('nome', nome);
     localStorage.setItem('senha', senha);
     localStorage.setItem('ambulatorio', ambulatorio);
+    localStorage.setItem('especialidade', especialidade);
     localStorage.setItem('urgencia', urgencia);
 
     window.open('painelChamada.html', '_blank');
